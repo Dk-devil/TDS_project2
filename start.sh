2de0b9a3b1f593fb82dd01eb85ce87e679e0ed12
DHANUSH R <128568369+Dk-devil@users.noreply.github.com>
2025-03-30 18:49:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+pip install --no-cache-dir -r requirements.txt  # Ensure dependencies are installed
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
